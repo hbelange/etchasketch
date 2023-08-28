ef1de09072d033e40e7a4b673ff0c57118b87158
hbelange <harrisonbelanger@gmail.com>
2023-08-28 16:28:09 -0400

    Created clear grid function, as well as create grid function

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const container = document.querySelector('.container'); // selects the body
 container.style.cssText = 'height: 900px; width: 900px; border: 2px solid black; display: flex; flex-wrap: wrap'
 
-// Loop for creating 16x16 div grid
-for (i = 0; i < 256; i++){
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.style.cssText = "border: 1px solid black; height: 54.25px; width: 54.25px"; // (900 - 2x1 - 15x2)/16
-    container.appendChild(square);
+// Clears every single square, removing its div element
+function clearGrid(){
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.remove());
 }
 
-// Creating the event listener for the hover effect,turns colour red
-const squares = document.querySelectorAll('.square'); // node list for all squares
-squares.forEach(square => square.addEventListener("mouseover", function (e){
-    e.target.classList.add("hover");
-}))
+// Loop for creating 16x16 div grid
+function createGrid(size){
+    clearGrid();
+    for (i = 0; i < size*size; i++){
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.cssText = `border: 1px solid black; height: ${(900-2-(size-1)*2)/size}px; width: ${(900-2-(size-1)*2)/size}px`; // (900 - 2x1 - 15x2)/16
+        container.appendChild(square);
 
+        // Creating the event listener for the hover effect,turns colour red
+        square.addEventListener('mouseover', function(e){
+            e.target.classList.add('hover');
+        })
+    }
+}
+
+// Button logic
+const button = document.querySelector('button');
+button.addEventListener("click", () => {
+    const size = Number(prompt("Size of new grid:"));
+    createGrid(size);
+})
+
+// Initial grid creation
+createGrid(16);
